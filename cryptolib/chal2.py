6bdd4b914ce4ac2381e69a0387035d77851f21c9
Christopher Greene <archgoon@gmail.com>
2024-03-20 22:13:48 -0700

    Remove chal files.
    I think this were from before me doing this via pytest.

--- cryptolib/chal2.py
+++
@@ -1,8 +0,0 @@
-from cryptolib import hexlify,unhexlify,strxor
-
-assert (hexlify(
-        strxor(
-            unhexlify(b"1c0111001f010100061a024b53535009181c"),
-            unhexlify(b"686974207468652062756c6c277320657965")))
-    ==b"746865206b696420646f6e277420706c6179")
-print("Success")
--- cryptolib/chal3.py
+++
@@ -1,3 +0,0 @@
-from cryptolib import unhexlify, brute_decrypt_sb_xor
-
-print(brute_decrypt_sb_xor(unhexlify(b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))[0])
